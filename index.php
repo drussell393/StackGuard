b13d2565a81ec0750e5a282263d80bb41b144876
Dave Russell <dave@createazure.com>
2015-07-27 10:23:17 -0400

    Update index.php
    Indent fix. This was just bugging me (: Also added <head> tag.

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
 <?php 
-
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 include 'modules/get_minions.php';
-
 ?>
 
 <html>
-<body>
-
-<link rel="stylesheet" href="css/style.css" type="text/css">
-
-<title>StackGuard</title>
-<div id="sg-nav">
-
-    <ul>
-        <?php get_minions(); ?>
-    </ul>
-
-</div>
-<div class="main-sg-panel">
-    <h1>Placeholder</h1>
-    <p>More interesting stuff will go here.</p>
-</div>
-</body>
+    <head>
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+        <title>StackGuard</title>
+    </head>
+    <body>
+        <div id="sg-nav">
+            <ul>
+                <?php get_minions(); ?>
+            </ul>
+        </div>
+        <div class="main-sg-panel">
+            <h1>Placeholder</h1>
+            <p>More interesting stuff will go here.</p>
+        </div>
+    </body>
 </html>
