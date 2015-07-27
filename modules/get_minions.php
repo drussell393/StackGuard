b4214c4d958cc6c0747732832cafbb1c8d6edce5
Dave Russell <dave@createazure.com>
2015-07-27 10:21:07 -0400

    Update get_minions.php
    PHP should not have indents after the opening tag.

--- modules/get_minions.php
+++ modules/get_minions.php
@@ -1,12 +1,10 @@
 <?php
-
-    function get_minions() {
-        $minions_list = shell_exec('sudo ls -1 /etc/salt/pki/master/minions');
-        $minions_list = explode("\n", $minions_list);
-        array_pop($minions_list);
-        foreach($minions_list as $minion) {
-            echo "        <li>".$minion."</li>\n" ;
-        }
+function get_minions() {
+    $minions_list = shell_exec('sudo ls -1 /etc/salt/pki/master/minions');
+    $minions_list = explode("\n", $minions_list);
+    array_pop($minions_list);
+    foreach($minions_list as $minion) {
+        echo "        <li>".$minion."</li>\n" ;
     }
-
+}
 ?>
